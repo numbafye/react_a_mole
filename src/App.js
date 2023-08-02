eb8f3136bbf9304d406f786ff0ed98bfb90d5cd6
numbafye <saadiqgp@gmail.com>
2023-08-01 22:13:46 -0400

    mole container, mole and empty slots completed. score system works. game complete.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import { useState } from "react";
 import MoleContainer from "./MoleContainer";
 
--- src/MoleContainer.js
+++ src/MoleContainer.js
@@ -1,13 +1,28 @@
-// Just one example
+import { useState } from "react";
 import Mole from "./Mole";
+import EmptySlot from "./EmptySlot";
 
-function MoleContainer(props) {
-  return (
-    <div>
-      <h2> Mole Container </h2>
-      <Mole />
-    </div>
+const MoleContainer = (props) => {
+  let [theMole, setTheMole] = useState(false);
+
+  const handleClick = (e) => {
+    props.setScore(props.score + 1);
+    setTheMole(false);
+  };
+
+  let displayMole = theMole ? (
+    <Mole
+      setScore={props.setScore}
+      toggle={setTheMole}
+      handleClick={handleClick}
+    />
+  ) : (
+    <EmptySlot toggle={setTheMole} />
   );
-}
+
+  return (
+    <div style={{ display: "inline-block", width: "30vw" }}>{displayMole}</div>
+  );
+};
 
 export default MoleContainer;
