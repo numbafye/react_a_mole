623e473dcbef817cefc9bf36dfc9e016a8d749fb
numbafye <saadiqgp@gmail.com>
2023-08-01 21:25:17 -0400

    app setup with components created

---
+++ src/MoleContainer.js
@@ -0,0 +1,13 @@
+// Just one example
+import Mole from "./Mole";
+
+function MoleContainer(props) {
+  return (
+    <div>
+      <h2> Mole Container </h2>
+      <Mole />
+    </div>
+  );
+}
+
+export default MoleContainer;
